38a14f5cd3f26c38c009ab6499bd952a0775d856
blackmiaool <blackmiaool@gmail.com>
2015-10-01 01:41:40 +0800

    mv header to bilibili style

--- wp-content/themes/kexie/header.php
+++ wp-content/themes/kexie/header.php
@@ -24,11 +24,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-    <div id="kx-header">
-        <div class="wrap">
-         <a href="/" class="brand">IC科协</a><a   href="/" class="item"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;首页</a><a   href="/mediawiki/" class="item"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>&nbsp;&nbsp;资料库</a><a  href="/message/" class="item"><span class="glyphicon glyphicon-blackboard" aria-hidden="true"></span>&nbsp;留言板</a><a  href="/schoolmates/" class="item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;学长们</a>
-        </div>       
-    </div>
+
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
 	<div id="sidebar" class="sidebar">
 		<header id="masthead" class="site-header" role="banner">
@@ -51,5 +47,17 @@
 
 		<?php get_sidebar(); ?>
 	</div><!-- .sidebar -->
-
+    <div id="kx-header">
+        <div class="wrap">
+        <ul>
+            <li><a href="/" class="brand">IC科协</a></li>
+            <li><a   href="/" class="item selected"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;首页</a></li>
+            <li><a   href="/mediawiki/" class="item "><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>&nbsp;&nbsp;资料库</a></li>
+            <li><a  href="/message/" class="item"><span class="glyphicon glyphicon-blackboard" aria-hidden="true"></span>&nbsp;留言板</a></li>
+            <li><a  href="/schoolmates/" class="item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;学长们</a></li>
+            <li><span class="others"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></span></li>
+        </ul>
+         
+        </div>       
+    </div>
 	<div id="content" class="site-content">
